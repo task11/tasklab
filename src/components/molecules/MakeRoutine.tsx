f816958ff9f3edfe67ba547416bd675ba03130a5
task11 <task11@naver.com>
2024-02-26 15:44:56 +0900

    feat: Add Make routine button

--- src/components/molecules/MakeRoutine.tsx
+++ src/components/molecules/MakeRoutine.tsx
@@ -1,9 +1,13 @@
 import Atom from "../atoms";
 
+import PlusButton from "../../../public/assets/svgs/PlusButton.svg";
+
 export default function MakeRoutine() {
   return (
     <Atom.Card variant="skeleton">
-      <div>추가</div>
+      <div className="w-full h-full flex items-center justify-center">
+        <PlusButton />
+      </div>
     </Atom.Card>
   );
 }
