2ee5dac08a0764644955545c1f3ed47a7fc7f0a8
task11 <task11@naver.com>
2024-02-26 12:48:02 +0900

    feat: Modify protected router logic

--- src/utils/auth/WithAuth.tsx
+++ src/utils/auth/WithAuth.tsx
@@ -4,16 +4,18 @@ import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 export default function WithAuth(Component: React.FC) {
-  const token =
-    typeof window !== "undefined" ? localStorage.getItem("accessToken") : null;
   return function WithAuth(props: any) {
+    // const token =
+    // typeof window !== "undefined"
+    //   ? localStorage.getItem("accessToken")
+    //   : null;
     useEffect(() => {
-      if (!token) {
+      if (!localStorage.getItem("accessToken")) {
         redirect("/landing");
       }
     }, []);
 
-    if (!token) {
+    if (typeof window !== "undefined" && !localStorage.getItem("accessToken")) {
       return null;
     }
 
