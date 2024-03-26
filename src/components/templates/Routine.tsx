3bfa6c219292653828496fc0984c1fdf99624df4
task11 <task11@naver.com>
2024-03-26 16:19:14 +0900

    style: chagne tailwindcss syntax

--- src/components/templates/Routine.tsx
+++ src/components/templates/Routine.tsx
@@ -2,7 +2,7 @@ import Organism from "@/components/organisms";
 
 export default function Routine() {
   return (
-    <div className="relative w-full h-screen py-58px bg-primary_100 flex flex-col gap-4">
+    <div className="relative w-full h-screen flex flex-col gap-4 py-58px bg-primary_100 ">
       <Organism.Header />
       <Organism.RoutineCalendar />
       <section className="h-full relative">
